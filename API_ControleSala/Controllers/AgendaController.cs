8c67f973f0aab4a910a22adb3a1cc61f21c2afac
EMERSON DE JESUS CANDIDO <emerson.jcandido1@senacsp.edu.br>
2022-12-08 19:29:17 -0300

    Adicionar arquivos de projeto.

---
+++ API_ControleSala/Controllers/AgendaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_ControleSala.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgendaController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Cont_Agenda(string o_Agenda)
+        {
+
+            string ChaveConexao = "Data Source=10.39.45.44; Initial Catalog=BancoProjetoER; User ID=Turma2022;Password=Turma2022@2022";
+
+            DataSet DataSetAgenda = new DataSet();
+
+            SqlConnection Conexao = new SqlConnection(ChaveConexao);
+            Conexao.Open();
+            string wQuery = $"select * from Agenda where ID like '{o_Agenda}'";
+            SqlDataAdapter adapter = new SqlDataAdapter(wQuery, Conexao);
+            adapter.Fill(DataSetAgenda);
+
+            string json = JsonConvert.SerializeObject(DataSetAgenda, Formatting.Indented);
+
+            return json;
+        }
+        [HttpPost]
+        public string Cont_Agenda2(string o_Agenda)
+        {
+
+            string ChaveConexao = "Data Source=10.39.45.44; Initial Catalog=BancoProjetoER; User ID=Turma2022;Password=Turma2022@2022";
+
+            DataSet DataSetAgenda = new DataSet();
+
+            SqlConnection Conexao = new SqlConnection(ChaveConexao);
+            Conexao.Open();
+            string wQuery = $"select * from Agenda where ID like '%{o_Agenda}%'";
+            SqlDataAdapter adapter = new SqlDataAdapter(wQuery, Conexao);
+            adapter.Fill(DataSetAgenda);
+
+            string json = JsonConvert.SerializeObject(DataSetAgenda, Formatting.Indented);
+
+            return json;
+        }
+    }
+}
---
+++ API_ControleSala/Controllers/SalaController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_ControleSala.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalaController : ControllerBase
+    {
+        [HttpGet]
+        public string Cont_Sala(string o_Sala)
+        {
+
+            string ChaveConexao = "Data Source=10.39.45.44; Initial Catalog=BancoProjetoER; User ID=Turma2022;Password=Turma2022@2022";
+
+            DataSet DataSetSala = new DataSet();
+
+            SqlConnection Conexao = new SqlConnection(ChaveConexao);
+            Conexao.Open();
+            string wQuery = $"select * from Sala where ID like '%{o_Sala}%'";
+            SqlDataAdapter adapter = new SqlDataAdapter(wQuery, Conexao);
+            adapter.Fill(DataSetSala);
+
+            string json = JsonConvert.SerializeObject(DataSetSala, Formatting.Indented);
+
+            return json;
+        }
+        [HttpPost]
+        public string Cont_Sala2(string o_Sala)
+        {
+
+            string ChaveConexao = "Data Source=10.39.45.44; Initial Catalog=BancoProjetoER; User ID=Turma2022;Password=Turma2022@2022";
+
+            DataSet DataSetSala = new DataSet();
+
+            SqlConnection Conexao = new SqlConnection(ChaveConexao);
+            Conexao.Open();
+            string wQuery = $"select * from Sala where ID like '%{o_Sala}%'";
+            SqlDataAdapter adapter = new SqlDataAdapter(wQuery, Conexao);
+            adapter.Fill(DataSetSala);
+
+            string json = JsonConvert.SerializeObject(DataSetSala, Formatting.Indented);
+
+            return json;
+        }
+    }
+}
